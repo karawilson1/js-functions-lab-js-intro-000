7b8809533d94f57c45817798c6427fc8bb4958d0
Wilson <karaewilson16@gmail.com>
2021-01-07 17:44:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function happyHolidays(string) {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(Kara) {
-  return `Happy holidays, ${Kara}`
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}`
 }
